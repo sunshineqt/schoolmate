7b6a17557d664c93f104f556e0ff292f2e56898d
sunshineqt <m17855847521@163.com>
2017-06-05 22:19:44 +0800

    Update index1.js

--- schoolmate/fangxyb/js/index1.js
+++ schoolmate/fangxyb/js/index1.js
@@ -1,3 +1,47 @@
+/*轮播图*/  
+var imgs=document.getElementById('imgs').getElementsByTagName('li');
+       var nums=document.getElementById("num").getElementsByTagName("li");
+       var luanpo=document.getElementById("luanpo");
+       var oimg=document.getElementById('imgs');
+       var iNow=0;
+       var dt=null;
+       oimg.style.width=imgs.length*imgs[0].offsetWidth+"px";
+
+       function tab(){
+           for(var i=0;i<nums.length;i++){
+               nums[i].index=i;
+               nums[i].onclick=function(){
+                   clearInterval(dt);
+                   iNow=this.index;
+                   for(var i=0;i<nums.length;i++){
+                       nums[i].className="b";
+                   }
+                   this.className="a";
+                   oimg.style.left=-(imgs[0].offsetWidth*iNow)+"px";  
+               }
+               nums[i].onmouseout=function(){
+                   start();
+               }
+           }
+       }
+
+       function start(){
+           clearInterval(dt);
+           dt=setInterval(function(){
+               if(iNow>nums.length-2){
+                   iNow=0;
+               }else{
+                   iNow++;
+               }
+               for(var k=0;k<nums.length;k++){
+                   if(iNow==nums[k].index){nums[k].className='a';}else{nums[k].className='b';}
+               }
+               oimg.style.left=-(imgs[0].offsetWidth*iNow)+"px";    //这边可以加上我前面所写的那个缓动框架
+           },1000);
+           tab();
+       }
+       start();
+
 $(document).ready(function(){
   $("#llist1").click(function(){
     $("#llist1").css("background","pink");
